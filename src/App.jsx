8a7cadd2dc698ff7d56b8f175fa8e8c83e57646a
manzaero <ligoero@gmail.com>
2024-11-12 22:25:10 +0300

    first commit for json-server from second task

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import styles from './app.module.css'
 import {useEffect, useState} from "react";
 
 
-const urlTodos = `https://jsonplaceholder.typicode.com/todos/${1}`
+const urlTodos = `http://localhost:3000/todos/`
 
-function App() {
+export const App = function () {
     const [todos, setTodos] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         fetch(urlTodos)
@@ -14,17 +15,41 @@ function App() {
                 setTodos(todo)
                 console.log(todo)
             })
+            .finally(() => {
+                setLoading(false)
+            })
     }, [])
 
+    const addTodo = () => {
+        fetch(urlTodos, {
+            method: 'POST',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                "title": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type sp",
+                "completed": false
+            })
+        })
+            .then(todo => todo.json())
+            .then(todo => {
+                console.log(todo)
+            })
+    }
+
     return (<>
-            <div className={styles.container}>
-                <h2>List todos</h2>
-                <ul>
-                    <li>{todos.title}</li>
-                </ul>
-                <button className={styles.btn}>Add</button>
-            </div>
-        </>)
+        <div className={styles.container}>
+            <h2>List todos</h2>
+            {loading ? <div className={styles.loader}></div> : todos.length === 0 ?
+                <div className={styles.wrong}>Todos not found</div>
+                : <ul>
+                    {todos.map((todo) => (
+                        <li key={todo.id}>{todo.title}</li>
+                    ))}
+                </ul>}
+            <button
+                className={styles.btn}
+                onClick={addTodo}
+            >Add</button>
+        </div>
+    </>)
 }
 
-export default App
